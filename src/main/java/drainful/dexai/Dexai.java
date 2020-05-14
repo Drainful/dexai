5e61723949012d23b864148184555eae483d4470
Adrian Fullmer <adrianfullmer@yahoo.com>
2020-05-14 14:23:04 -0500

    inital commit

---
+++ src/main/java/drainful/dexai/Dexai.java
@@ -0,0 +1,19 @@
+package drainful.dexai;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Hello world!
+*/
+@Mod("dexai")
+public class Dexai {
+    public static final String MODID = "dexai";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
